c10e9ab75bbb152bd11b36da7e5c946672ddd190
Luke <117487478+lb1227@users.noreply.github.com>
2025-03-25 18:54:16 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const games = {
+  "Spyro's Adventure": [
+    { name: "Spyro", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/Spyro1.png" },
+    { name: "Trigger Happy", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/TriggerHappy1.png" },
+    { name: "Stealth Elf", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/StealthElf1.png" },
+    { name: "Gill Grunt", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/GillGrunt1.png" },
+    { name: "Eruptor", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/Eruptor1.png" },
+  ],
+  "Giants": [
+    { name: "Bouncer", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/Bouncer.png" },
+    { name: "Crusher", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/Crusher.png" },
+    { name: "Hot Head", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/HotHead.png" },
+    { name: "Swarm", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/Swarm.png" },
+    { name: "Tree Rex", image: "https://skylanderscharacterlist.com/wp-content/uploads/2013/02/TreeRex.png" },
+  ]
+};
